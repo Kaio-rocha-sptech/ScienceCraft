daaf2e4f55c6b6f337f6b68af70d540367c0b70b
kaio-rocha-sptech <kaio.rocha@sptech.school>
2025-11-26 00:40:03 -0300

    final.

--- ScienceCraft/src/routes/convite.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var conviteController = require("../controllers/conviteController"); // << caminho correto?
-
-router.get("/quantidade/:idUsuario", conviteController.obterQuantidade);
-router.get("/ranking/total", conviteController.rankingTotal);
-
-module.exports = router;
